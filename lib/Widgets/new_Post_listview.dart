1932e6fc06195ee06619a1d2a1b470be0538fb50
ahmed <atef87133@gmail.com>
2025-04-21 08:41:26 +0200

    ahmedd

--- lib/Widgets/new_Post_listview.dart
+++ lib/Widgets/new_Post_listview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:newss/models/article_model.dart';
+
 import 'news_Post.dart';
 
 class NewPostListview extends StatelessWidget {
